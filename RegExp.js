9669eab85332313687d878a793c9e8f38577510d
RahulAid <raccess97@gmail.com>
2023-01-19 15:39:23 +0530

    Regular-Expression

---
+++ RegExp.js
@@ -0,0 +1,7 @@
+// Regular-Expression
+
+const regExpDate = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+const match      = regExpDate.exec('2020-06-25');
+const year       = match[1]; // 2020
+const month      = match[2]; // 06
+const day        = match[3]; // 25
